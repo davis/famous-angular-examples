b49529ccdee72b9c103ea0692fc4045ef8fc2993
Davis Kim <dchk90@gmail.com>
2014-06-25 23:29:36 -0700

    clean-up flipper

--- app/scripts/controllers/flipper.js
+++ app/scripts/controllers/flipper.js
@@ -25,8 +25,7 @@ angular.module('integrationApp')
     inBack800:         { curve: Easing.inBack, duration: 800 },
     inOutSine500:      { curve: Easing.inOutSine, duration: 500 },
     outBack250:        { curve: Easing.outBack, duration: 250 },
-    springTransition:  { method: "spring", period: 1500, dampingRatio: 0.8, velocity: 0.003 },
-    springTransition2: { method: "spring", period: 1500, dampingRatio: 0.8, velocity: 0.004 }
+    springTransition:  { method: "spring", period: 1100, dampingRatio: 0.7, velocity: 0.004 },
   };
 
   // initialize transitionables
@@ -37,8 +36,8 @@ angular.module('integrationApp')
 
   // states used by template
   $scope.states = {
-    screenTranslate: function() { return [620 * t9ables.screenTranslate.get() - 310, 0, 2]; },
-    clockTranslate: function() { return [0, 200 * t9ables.clocktranslate.get(), 2]; },
+    screenTranslate: function() { return [620 * t9ables.screenTranslate.get() - 310, 0, 0]; },
+    clockTranslate: function() { return [0, 200 * t9ables.clocktranslate.get(), 0]; },
   };
 
   // define icons
@@ -47,10 +46,10 @@ angular.module('integrationApp')
   for(var i = 0; i < icons; i++) {
     (function(i){
       $scope.icons[i] = {
-        X: 75*i + 17,
+        X: -113 + 75*i,
         Y: new Transitionable(0),
         translate: function() {
-          return [$scope.icons[i].X, 550 - ( 70 * $scope.icons[i].Y.get() ), 2];
+          return [$scope.icons[i].X, 305 - ( 70 * $scope.icons[i].Y.get() ), 0];
         }
       };
     })(i);
@@ -58,25 +57,25 @@ angular.module('integrationApp')
 
   // icon bg colors
   $scope.icons[0].bgCol = '#FA726C';
-  $scope.icons[1].bgCol = '#BAD174';
+  $scope.icons[1].bgCol = '#FF9059';
   $scope.icons[2].bgCol = '#3BA686';
   $scope.icons[3].bgCol = '#5F6F8C';
 
   // icon icons
-  $scope.icons[0].icon = 'fa fa-phone';
-  $scope.icons[1].icon = 'fa fa-envelope';
-  $scope.icons[2].icon = 'fa fa-cloud';
-  $scope.icons[3].icon = 'fa fa-camera';
+  $scope.icons[0].icon = 'fa fa-phone icon';
+  $scope.icons[1].icon = 'fa fa-envelope icon';
+  $scope.icons[2].icon = 'fa fa-cloud icon';
+  $scope.icons[3].icon = 'fa fa-camera icon';
 
   $scope.inTransitionFunction = function (done) {
     Timer.setTimeout(function() {
       // set screen
-      t9ables.screenTranslate.set(0.5, TRANSITIONS.springTransition, function() {
+      t9ables.screenTranslate.set(0.5, TRANSITIONS.inBack800, function() {
         // set icons
         for(var i = 0; i < icons; i++) {
           (function(i) {
             Timer.setTimeout(function() {
-              $scope.icons[i].Y.set(1, TRANSITIONS.springTransition2, function() {
+              $scope.icons[i].Y.set(1, TRANSITIONS.springTransition, function() {
                 if(i === icons-1) done();
               });
             }, 100*i);
